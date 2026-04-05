c682be2859ed68dd2bd6b8b78ac3113c562694b3
Ryan Turner <100864486+Turnlings@users.noreply.github.com>
2026-04-06 00:10:39 +0100

    add project search bar

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,22 +1,57 @@
 import { useEffect, useState } from 'react';
 import { getProjects } from '../lib/getProjects';
 import ProjectCard from './ProjectCard';
-import { Container, Grid, GridCol } from '@mantine/core';
+import { ActionIcon, Container, Grid, GridCol, TextInput } from '@mantine/core';
+import { IconArrowRight, IconSearch } from '@tabler/icons-react';
 
 export default function Projects() {
   const [projects, setProjects] = useState<any[]>([]);
+  const [filteredProjects, setFilteredProjects] = useState<any[]>([]);
+  const [searchTerm, setSearchTerm] = useState("");
 
   useEffect(() => {
     getProjects().then(setProjects);
   }, []);
 
+  useEffect(() => {
+    setFilteredProjects(
+      projects.filter((p) =>
+        p.title.toLowerCase().includes(searchTerm.trim().toLowerCase()) ||
+        p.tech.some((tech: { name: string }) =>
+          tech.name.toLowerCase().includes(searchTerm.trim().toLowerCase())
+        )
+      )
+    )
+  }, [projects, searchTerm])
+
   return (
+    <>
+    <TextInput
+      radius="xl"
+      size="md"
+      placeholder="Search projects"
+      rightSectionWidth={42}
+      leftSection={<IconSearch size={18} stroke={1.5} />}
+      rightSection={
+        <ActionIcon
+          size={32}
+          radius="xl"
+          variant="filled"
+          aria-label="Search"
+        >
+          <IconArrowRight size={18} stroke={1.5} />
+        </ActionIcon>
+      }
+      aria-label="Search projects"
+      onChange={(event) => setSearchTerm(event.currentTarget.value)}
+    />
     <Grid>
-      {projects.map((p) => (
+      {filteredProjects.map((p) => (
         <GridCol key={p.slug} span={4}>
           <ProjectCard project={p}></ProjectCard>
         </GridCol>
       ))}
     </Grid>
+    </>
   );
 }
